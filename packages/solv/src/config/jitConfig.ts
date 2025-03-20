f269a2be8a234ca6bceb2d1a421cef68fde8c621
Gabriel Hicks <60371583+gabrielhicks@users.noreply.github.com>
2025-03-20 17:05:36 -0500

    update with new versions

--- packages/solv/src/config/jitConfig.ts
+++ packages/solv/src/config/jitConfig.ts
@@ -9,8 +9,8 @@ export interface JitoConfig {
 }
 
 export const JITO_CONFIG: JitoConfig = {
-  version: '2.1.14',
-  tag: 'v2.1.14',
+  version: '2.1.16',
+  tag: 'v2.1.16',
   commissionBps: 1000,
   relayerUrl: 'https://frankfurt.mainnet.block-engine.jito.wtf',
   blockEngineUrl: 'http://frankfurt.mainnet.relayer.jito.wtf:8100',
