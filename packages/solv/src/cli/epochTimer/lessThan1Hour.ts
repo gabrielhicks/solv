bcf7132683a22ffa7434a6125eb79fd0f111fc6f
Gabriel Hicks <60371583+gabrielhicks@users.noreply.github.com>
2025-06-05 17:14:08 -0500

    test it

--- packages/solv/src/cli/epochTimer/lessThan1Hour.ts
+++ packages/solv/src/cli/epochTimer/lessThan1Hour.ts
@@ -1,12 +1,12 @@
 import { EpochData } from './epochTimer'
-import { spawnSync } from 'child_process'
+// import { spawnSync } from 'child_process'
 import writeEpochDataToFile from './writeEpochDataToFile'
 // import alertMessage from './alertMessage'
-import chalk from 'chalk'
-import randomSleep from './randomSleep'
+// import chalk from 'chalk'
+// import randomSleep from './randomSleep'
 import { EpochInfoCLIType } from '@/lib/getEpochInfoByRust'
 import { DefaultConfigType } from '@/config/types'
-import { Network } from '@/config/enums'
+// import { Network } from '@/config/enums'
 
 const lessThan1Hour = async (
   totalMinutes: number,
@@ -14,25 +14,25 @@ const lessThan1Hour = async (
   currentEpoch: EpochInfoCLIType,
   config: DefaultConfigType,
 ) => {
-  const isMEV = config.IS_MEV_MODE
-  const isMainnet = config.NETWORK === Network.MAINNET
+  // const isMEV = config.IS_MEV_MODE
+  // const isMainnet = config.NETWORK === Network.MAINNET
   if (totalMinutes < 60 && !epochData.isLessThan1Hour) {
     // Update the database and send a notification
     await writeEpochDataToFile({ ...epochData, isLessThan1Hour: true })
     // await alertMessage(currentEpoch, '1 Hour', config)
 
     // If MEV is enabled, run `solv harvest` command
-    if (isMEV && isMainnet) {
+    // if (isMEV && isMainnet) {
       // Random Sleep to avoid network congestion
-      const waitTime = await randomSleep(1, 100)
-      console.log(
-        chalk.white(
-          `⏳ Waiting for ${waitTime} seconds before running solv harvest...`,
-        ),
-      )
+      // const waitTime = await randomSleep(1, 100)
+      // console.log(
+      //   chalk.white(
+      //     `⏳ Waiting for ${waitTime} seconds before running solv harvest...`,
+      //   ),
+      // )
       // run `solv harvest` command
-      spawnSync('solv', ['harvest'], { stdio: 'inherit', shell: true })
-    }
+      // spawnSync('solv', ['harvest'], { stdio: 'inherit', shell: true })
+    // }
     return true
   }
   return false
