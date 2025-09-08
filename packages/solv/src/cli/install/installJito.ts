47775f6f6b58aa3de7cc142fab55df8b6d75c0ea
Gabriel Hicks <60371583+gabrielhicks@users.noreply.github.com>
2025-09-08 14:33:44 -0500

    bump

--- packages/solv/src/cli/install/installJito.ts
+++ packages/solv/src/cli/install/installJito.ts
@@ -5,58 +5,13 @@ export const installJito = (version = VERSION_JITO_MAINNET, mod = false, isMajor
   if(isMajorThree) {
     if(mod) {
       const tag = `v${version}-mod`
-      spawnSync(`mkdir /tmp/${tag}`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(`cd /tmp/${tag}`, {
-        shell: true,
-        stdio: 'inherit',
-      })
       spawnSync(
-        `git -C /tmp/${tag} clone https://github.com/gabrielhicks/jito-solana.git --recurse-submodules .`,
+        `sh -c "$(curl --netrc-optional -sSfL https://raw.githubusercontent.com/gabrielhicks/jito-solana/v${tag}/installer)"`,
         {
           shell: true,
           stdio: 'inherit',
         },
       )
-      spawnSync(`git -C /tmp/${tag} checkout ${tag}`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(`git -C /tmp/${tag} submodule update --init --recursive`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(
-        `CI_COMMIT=$(git -C /tmp/${tag} rev-parse HEAD) /tmp/${tag}/scripts/cargo-install-all.sh /home/solv/.local/share/solana/install/releases/${tag}`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-      spawnSync(
-        `sudo rm -rf /home/solv/.local/share/solana/install/active_release/bin/`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-      spawnSync(
-        `cp -r /home/solv/.local/share/solana/install/releases/${tag}/bin/ /home/solv/.local/share/solana/install/active_release/bin/`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-      spawnSync(`sudo rm -rf /tmp/${tag}`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(`sudo systemctl daemon-reload`, {
-        shell: true,
-        stdio: 'inherit',
-      })
     } else {
       const tag = `v${version}-jito`
       spawnSync(`mkdir /tmp/${tag}`, {
