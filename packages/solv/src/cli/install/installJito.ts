9c8801044f852b7c39aa0e2c55464c652487f7df
Gabriel Hicks <60371583+gabrielhicks@users.noreply.github.com>
2026-03-26 10:12:31 -0500

    update

--- packages/solv/src/cli/install/installJito.ts
+++ packages/solv/src/cli/install/installJito.ts
@@ -7,77 +7,75 @@ export const installJito = (
   isMajorThree = false,
   xdp = false,
 ) => {
-  if (isMajorThree) {
-    if (mod) {
+  if (mod) {
+    spawnSync(
+      `sh -c "$(curl --netrc-optional -sSfL https://raw.githubusercontent.com/gabrielhicks/jito-solana/${version}/installer)"`,
+      {
+        shell: true,
+        stdio: 'inherit',
+      },
+    )
+  } else {
+    spawnSync(`mkdir /tmp/${version}`, {
+      shell: true,
+      stdio: 'inherit',
+    })
+    spawnSync(`cd /tmp/${version}`, {
+      shell: true,
+      stdio: 'inherit',
+    })
+    spawnSync(
+      `git -C /tmp/${version} clone https://github.com/jito-foundation/jito-solana.git --recurse-submodules .`,
+      {
+        shell: true,
+        stdio: 'inherit',
+      },
+    )
+    spawnSync(`git -C /tmp/${version} checkout ${version}`, {
+      shell: true,
+      stdio: 'inherit',
+    })
+    spawnSync(`git -C /tmp/${version} submodule update --init --recursive`, {
+      shell: true,
+      stdio: 'inherit',
+    })
+    spawnSync(
+      `CI_COMMIT=$(git -C /tmp/${version} rev-parse HEAD) /tmp/${version}/scripts/cargo-install-all.sh /home/solv/.local/share/solana/install/releases/${version}`,
+      {
+        shell: true,
+        stdio: 'inherit',
+      },
+    )
+    spawnSync(
+      `sudo rm -rf /home/solv/.local/share/solana/install/active_release`,
+      {
+        shell: true,
+        stdio: 'inherit',
+      },
+    )
+    spawnSync(
+      `sudo ln -sfn /home/solv/.local/share/solana/install/releases/${version} /home/solv/.local/share/solana/install/active_release`,
+      {
+        shell: true,
+        stdio: 'inherit',
+      },
+    )
+    if (xdp) {
       spawnSync(
-        `sh -c "$(curl --netrc-optional -sSfL https://raw.githubusercontent.com/gabrielhicks/jito-solana/${version}/installer)"`,
+        `sudo setcap cap_net_raw,cap_net_admin,cap_bpf,cap_perfmon=p /home/solv/.local/share/solana/install/active_release/bin/agave-validator`,
         {
           shell: true,
           stdio: 'inherit',
         },
       )
-    } else {
-      spawnSync(`mkdir /tmp/${version}`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(`cd /tmp/${version}`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(
-        `git -C /tmp/${version} clone https://github.com/jito-foundation/jito-solana.git --recurse-submodules .`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-      spawnSync(`git -C /tmp/${version} checkout ${version}`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(`git -C /tmp/${version} submodule update --init --recursive`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(
-        `CI_COMMIT=$(git -C /tmp/${version} rev-parse HEAD) /tmp/${version}/scripts/cargo-install-all.sh /home/solv/.local/share/solana/install/releases/${version}`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-      spawnSync(
-        `sudo rm -rf /home/solv/.local/share/solana/install/active_release`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-      spawnSync(
-        `sudo ln -sfn /home/solv/.local/share/solana/install/releases/${version} /home/solv/.local/share/solana/install/active_release`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-      if (xdp) {
-        spawnSync(
-          `sudo setcap cap_net_raw,cap_net_admin,cap_bpf,cap_perfmon=p /home/solv/.local/share/solana/install/active_release/bin/agave-validator`,
-          {
-            shell: true,
-            stdio: 'inherit',
-          },
-        )
-      }
-      spawnSync(`sudo rm -rf /tmp/${version}`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(`sudo systemctl daemon-reload`, {
-        shell: true,
-        stdio: 'inherit',
-      })
     }
+    spawnSync(`sudo rm -rf /tmp/${version}`, {
+      shell: true,
+      stdio: 'inherit',
+    })
+    spawnSync(`sudo systemctl daemon-reload`, {
+      shell: true,
+      stdio: 'inherit',
+    })
   }
 }
--- packages/solv/src/cli/update/bamUpdate.ts
+++ packages/solv/src/cli/update/bamUpdate.ts
@@ -6,91 +6,71 @@ export const bamUpdate = (
   isMajorThree = false,
   xdp = false,
 ) => {
-  if (isMajorThree) {
-    if (mod) {
-      spawnSync(
-        `sh -c "$(curl --netrc-optional -sSfL https://raw.githubusercontent.com/gabrielhicks/jito-solana/${tag}/installer)"`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-    } else {
-      spawnSync(`mkdir /tmp/${tag}`, {
+  if (mod) {
+    spawnSync(
+      `sh -c "$(curl --netrc-optional -sSfL https://raw.githubusercontent.com/gabrielhicks/jito-solana/${tag}/installer)"`,
+      {
         shell: true,
         stdio: 'inherit',
-      })
-      spawnSync(`cd /tmp/${tag}`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(
-        `git clone https://github.com/jito-foundation/jito-solana.git --recurse-submodules /tmp/${tag}`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-      spawnSync(`git -C /tmp/${tag} checkout ${tag}`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(`git -C /tmp/${tag} submodule update --init --recursive`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-      spawnSync(
-        `CI_COMMIT=$(git -C /tmp/${tag} rev-parse HEAD) /tmp/${tag}/scripts/cargo-install-all.sh /home/solv/.local/share/solana/install/releases/${tag}`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-      spawnSync(
-        `sudo rm -rf /home/solv/.local/share/solana/install/active_release`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-      spawnSync(
-        `sudo ln -sfn /home/solv/.local/share/solana/install/releases/${tag} /home/solv/.local/share/solana/install/active_release`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-      if (xdp) {
-        spawnSync(
-          `sudo setcap cap_net_raw,cap_net_admin,cap_bpf,cap_perfmon=p /home/solv/.local/share/solana/install/active_release/bin/agave-validator`,
-          {
-            shell: true,
-            stdio: 'inherit',
-          },
-        )
-      }
-      spawnSync(`sudo rm -rf /tmp/${tag}`, {
-        shell: true,
-        stdio: 'inherit',
-      })
-    }
+      },
+    )
   } else {
-    if (mod) {
+    spawnSync(`mkdir /tmp/${tag}`, {
+      shell: true,
+      stdio: 'inherit',
+    })
+    spawnSync(`cd /tmp/${tag}`, {
+      shell: true,
+      stdio: 'inherit',
+    })
+    spawnSync(
+      `git clone https://github.com/jito-foundation/jito-solana.git --recurse-submodules /tmp/${tag}`,
+      {
+        shell: true,
+        stdio: 'inherit',
+      },
+    )
+    spawnSync(`git -C /tmp/${tag} checkout ${tag}`, {
+      shell: true,
+      stdio: 'inherit',
+    })
+    spawnSync(`git -C /tmp/${tag} submodule update --init --recursive`, {
+      shell: true,
+      stdio: 'inherit',
+    })
+    spawnSync(
+      `CI_COMMIT=$(git -C /tmp/${tag} rev-parse HEAD) /tmp/${tag}/scripts/cargo-install-all.sh /home/solv/.local/share/solana/install/releases/${tag}`,
+      {
+        shell: true,
+        stdio: 'inherit',
+      },
+    )
+    spawnSync(
+      `sudo rm -rf /home/solv/.local/share/solana/install/active_release`,
+      {
+        shell: true,
+        stdio: 'inherit',
+      },
+    )
+    spawnSync(
+      `sudo ln -sfn /home/solv/.local/share/solana/install/releases/${tag} /home/solv/.local/share/solana/install/active_release`,
+      {
+        shell: true,
+        stdio: 'inherit',
+      },
+    )
+    if (xdp) {
       spawnSync(
-        `sh -c "$(curl --netrc-optional -sSfL https://raw.githubusercontent.com/gabrielhicks/jito-solana/${tag}/installer)"`,
-        {
-          shell: true,
-          stdio: 'inherit',
-        },
-      )
-    } else {
-      spawnSync(
-        `sh -c "$(curl --netrc-optional -sSfL https://release.jito.wtf/${tag}/install)"`,
+        `sudo setcap cap_net_raw,cap_net_admin,cap_bpf,cap_perfmon=p /home/solv/.local/share/solana/install/active_release/bin/agave-validator`,
         {
           shell: true,
           stdio: 'inherit',
         },
       )
     }
+    spawnSync(`sudo rm -rf /tmp/${tag}`, {
+      shell: true,
+      stdio: 'inherit',
+    })
   }
 }
